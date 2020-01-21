f5c9599f924dfe62dbf2deaeee3775ee7f1b123d
Dmitry Filimonov <filimonovdmitry93@gmail.com>
2020-01-21 17:06:06 +0300

    update change2

--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.testkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.fragment_log_in)
+
+        val spinner : Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(this,R.array.users,android.R.layout.simple_spinner_item).also {
+            adapter ->  adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
 
     }
 }
